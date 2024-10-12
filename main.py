5fa52fa2d792412f39dc8578972a3663e282776b
duck0249 <donggeunlee@Donggeuns-MacBook-Air.local>
2024-10-12 11:07:16 +0700

    email sending code completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-import smtplib, ssl
+from send_email import send_email
 
 api_key = "7b3c5cd21e2a457db2a883ab08ca391a"
 url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-09-12&sortBy=publishedAt&apiKey={api_key}"
@@ -12,10 +12,17 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-# Access the article titles and description
-# print(content["articles"][0]["title"])
+# Treat strings to send email.
+titles_descriptions = ["Title : " + str(article["title"]) + "\n" + 
+					   "Description :" + str(article["description"]) + "\n\n" 
+					   for article in content["articles"][0:20]]
+message = "".join(titles_descriptions)
 
-for article in content["articles"]:
-	print(article["title"])
-	print(article["description"])
+# generate messages and setup to send email.
+username = "duck0249@gmail.com"
+subject = "Today Daily news"
+message = message
 
+# Send email
+send_email(username, subject, message)
+ 
